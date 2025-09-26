b29844fb35824093e8e6e7de060609d339d85a74
Ayrton Sab <ayrton_leo08@hotmail.com.ar>
2025-09-25 22:12:44 -0300

    Se implemetó las funciones: nuevo a listo, a suspendido

--- src/core/colas.py
+++ src/core/colas.py
@@ -1,7 +1,7 @@
 # Clase base para arrancar, si necesitas agregar mas cosas hacelo
 
 from .proceso import Proceso
-
+from gestor_memoria import GestorMemoria
 class GestorColas:
     def __init__(self):
         # TO DO: Inicializar las colas de estados
@@ -19,7 +19,7 @@ class GestorColas:
     # - Ordenar por tiempo de arribo 
     def agregar_nuevo(self, proceso: Proceso):
         '''
-            Agrega y ordena la lista de mayor a menor tiempo de irrupción
+            Agrega y ordena la lista de mayor a menor tiempo de irrupción.
         '''
         self.nuevos.append(proceso)
         self.nuevos.sort(key=lambda proceso: proceso.tiempo_irrupcion, reverse=True)
@@ -27,11 +27,26 @@ class GestorColas:
     
     # TO DO: Implementar método para mover proceso de nuevos a listos
     def nuevo_a_listo(self, proceso: Proceso):
-        pass
+        '''
+            Toma el primero de la lista de nuevos y lo mueve a la lista de listos.
+        '''
+        self.nuevos[0].estado = "Listo"
+        self.listos.append(self.nuevos[0])
+        self.listos.pop(0)
+
+
+
     
     # TO DO: Implementar método para mover proceso a suspendidos
     def a_suspendidos(self, proceso: Proceso):
-        pass
+        '''
+            Agrega el proceso a la cola de suspendidos.
+        '''
+        self.ejecucion.estado = "Suspendido"
+        self.suspendidos.append(self.ejecucion)
+        
+
+
     
     # TO DO: Implementar método para activar proceso suspendido
     # - Mover de suspendidos a listos si hay memoria disponible
