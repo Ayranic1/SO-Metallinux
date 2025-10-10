9d32d0bb15628fb5ca6ce7200dfcedf27276c0f0
Ayrton Sab <ayrton_leo08@hotmail.com.ar>
2025-10-09 21:26:46 -0300

    refacto: realizar código de encontrar_particion con política de asginación best-fit

--- src/core/gestor_memoriaBestFit.py
+++ src/core/gestor_memoriaBestFit.py
@@ -19,8 +19,20 @@ class GestorMemoriaBestFit(GestorMemoria):
 
     
     def encontrar_particion(self, tamaño_proceso):
-        pass
+        # se busca la partició acorde a la política de asignación best-fit
 
+        espacio_ideal = 550
+        for particion in self.particiones:
+
+            if particion.esta_libre():
+                espacio_libre = particion.tamaño - tamaño_proceso
+                if ((espacio_libre > 0) and (espacio_libre < espacio_ideal)):
+                    particion_ideal = particion
+                    espacio_ideal = espacio_libre
+        
+        if (espacio_ideal!=550):
+            return particion_ideal
+        else: return False
 
     # método para saber si alguna de las particiones está libre
     def hay_libre(self)-> bool:
@@ -28,8 +40,6 @@ class GestorMemoriaBestFit(GestorMemoria):
             if particion.disponible():
                 return True
         return False
-
-
 
 
 #  100K destinados al Sistema Operativo.
