b0fedc124f4065be4227ad8a1010c9ddd90a0855
Ayrton Sab <ayrton_leo08@hotmail.com.ar>
2025-11-26 23:01:38 -0300

    fix: Asignar el Sistema Operativo a la memoria

--- src/core/plantillas.py
+++ src/core/plantillas.py
@@ -22,9 +22,19 @@ class GestorMemoria(ABC):
     
     def inicializar_particiones(self):
         # TO DO: Crear e inicializar la lista de particiones
-        tams = [100, 250, 150, 50] # tamaños de particiones
+        tams = [250, 150, 50] # tamaños de particiones
         id=0
         ult_dir = 0
+
+        # partición e asignación del SO
+        t=100
+        particion = Particion(str(id),ult_dir, t)
+        particion.proceso_asignado = Proceso('SO', t, 0, 0)
+        self.particiones.append(particion)
+        ult_dir+= t
+        id+=1
+
+        # para las particiones para los procesos
         for t in tams:
             particion = Particion(str(id),ult_dir, t)
             self.particiones.append(particion)
